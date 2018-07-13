89358cd4a649cbbb640582df4280324976711b23
vipultoshniwal <39092368+vipultoshniwal@users.noreply.github.com>
2018-07-14 03:14:31 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
-
-
-
-
-
-url(r'^getpage/',views.Home_Logs)
+from django.conf.urls import url
+from django.contrib import admin
+from Monthly_Expenses_App import views
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^getpage/',views.Home_Logs)
+]
