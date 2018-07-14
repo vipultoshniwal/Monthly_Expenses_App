e727d1f982a4c546714b6658b0cf0a2054003a7f
vipultoshniwal <39092368+vipultoshniwal@users.noreply.github.com>
2018-07-14 20:21:24 +0530

    Update views.py

--- views.py
+++ views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def Home_Logs(request): 
     return render(request,"Home.html")
+
+def AddExpenses(request):
+    return render(request,"AddExpense.html")
+
+def update(request):
+    Username=request.POST["P1"]
+    Breakfast=request.POST["P2"]
+    Trip=request.POST["P3"]
+    Hangout=request.POST["P4"]
+    d=ExpenseCategory(Username=Username1,Breakfast= Breakfast1,Trip=Trip1,Hangout=Hangout1)
+    d.save()
+    return HttpResponse(request,"Home.html")
