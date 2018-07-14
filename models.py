89a78417377edc1546e483678fa0d12aa1b75ad9
vipultoshniwal <39092368+vipultoshniwal@users.noreply.github.com>
2018-07-14 20:24:24 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 
 class UserInfo(models.Model):
-    User_id = models.IntegerField(primary_key=True)
-    User_name =models.CharField(max_length=20)
+    Userid = models.IntegerField(primary_key=True)
+    Username =models.CharField(max_length=20)
    
 class ExpenseCategory(models.Model):
     Breakfast = models.IntegerField(max_length=15)
     Trip = models.IntegerField(max_length=15)
     Hangout = models.IntegerField(max_length=15)
-    User_name = models.ForeignKey(User_Info, on_delete=models.CASCADE)
+    Username = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
