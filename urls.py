2638c168e831c975c82894a7de030fbc07f18ba3
vipultoshniwal <39092368+vipultoshniwal@users.noreply.github.com>
2018-07-16 00:29:33 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
-from Monthly_Expenses_App import views
+from expense_record_app import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^show/',views.Home_Logs),
     url(r'^AddExpenses/',views.AddExpenses),
     url(r'^update/',views.update),
     url(r'^Login/',views.Login),
-    
-    
-]
+    url(r'^Sum/',views.calculation),
+    url(r'^Divide/',views.DivideExpense)
