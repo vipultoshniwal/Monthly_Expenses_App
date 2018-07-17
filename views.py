2708a71a833333e338cb8d4747f758111d4c6147
vipultoshniwal <39092368+vipultoshniwal@users.noreply.github.com>
2018-07-17 23:43:01 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,8 +1,9 @@
+
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
-#from .models import ExpenseCategory
+from .models import Expensecategories
 
 def Home_Logs(request): 
     return render(request,"Home.html")
@@ -15,9 +16,10 @@ def update(request):
     Breakfast=request.POST["P2"]
     Trip=request.POST["P3"]
     Hangout=request.POST["P4"]
-    d=ExpenseCategory(Username=Username,Breakfast= Breakfast,Trip=Trip,Hangout=Hangout)
+    d=Expensecategories(Username=Username,Breakfast= Breakfast,Trip=Trip,Hangout=Hangout)
     d.save()
-    return HttpResponse(request,"Home.html")
+    d1=Expensecategories.objects.all()
+    return render(request,"saved.html",{'d':d1})
 
 def Login(request):
     return render(request,"Login.html")
@@ -25,13 +27,13 @@ def Login(request):
 def calculation(request): 
     global total  
     Userid=request.POST["P5"]
-    r=ExpenseCategory.objects.get(Userid=userid) 
+    r=Expensecategories.objects.get(Userid=userid) 
     Breakfast=r.Breakfast
     Trip=r.Trip
     Hangout=r.Hangout
     total=Breakfast+Trip+Hangout
     P={'Breakfast':Breakfast,'Trip':Trip,'Hangout':Hangout,'total':sum}
-    return render(request,Sum.html",{'P':P})
+    return render(request,"Sum.html",{'P':P})
 
 def DivideExpense(request):
     Person=request.GET["P6"]
